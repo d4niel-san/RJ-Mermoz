79fcd6f14276f7d6d728baa174735b2d1a339833
d4niel-san <91570360+d4niel-san@users.noreply.github.com>
2022-01-31 15:15:00 -0300

    SCSS

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+
+export const NavBarMenu = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Pagina Principal</li>
+          <li>Productos</li>
+          <li>Redes</li>
+          <li>Contacto</li>
+          <li>Ingresar</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
