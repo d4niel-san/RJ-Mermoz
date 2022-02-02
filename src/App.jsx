cc7244a0b4ec9b768d14c178f031df35428731dc
d4niel-san <91570360+d4niel-san@users.noreply.github.com>
2022-02-02 12:40:24 -0300

    Listado de productos con Promesa

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import './styles/styles.scss';
 import ResponsiveAppBar from './Components/NavBar/ResponsiveAppBar';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './styles/theme';
+import HeadContainer from './Components/HeadContainer/HeadContainer';
+import { Item } from './Components/Item/Item';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ItemListContainer } from './Components/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-      <div>
-        <ThemeProvider theme={theme}>
-          <ResponsiveAppBar/>
-        </ThemeProvider>
-      </div>
+    <div>
+      <ThemeProvider theme={theme}>
+        <ResponsiveAppBar />
+        
+        <ItemListContainer/>
+      </ThemeProvider>
+    </div>
   );
 }
 
