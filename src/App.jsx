663cf99104f2778c6ff7f50fd95f10a125d6f0f5
d4niel-san <91570360+d4niel-san@users.noreply.github.com>
2022-02-17 14:24:57 -0300

    Carro y item detail terminados, falta cambiar cantidad

--- src/App.jsx
+++ src/App.jsx
@@ -7,48 +7,18 @@ import { ItemListContainer } from './Components/ItemListContainer/ItemListContai
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ItemDetailContainer } from './Components/ItemDetailContainer/ItemDetailContainer';
 import { Footer } from './Components/Footer/Footer';
-import { CartContext } from './Context/CartContext';
+import { CartContext, CartProvider } from './Context/CartContext';
 import { useState } from 'react';
 import { stock } from './data/stock';
+import { Cart } from './Components/Cart/Cart';
 
 function App() {
 
-  const [cart, setCart] = useState([])
 
-  const agregarAlCarrito = (item) => {
-    if (!isInCart(item.id)){
-      setCart( [...cart,item ])
-    } else {
-      if ((cart.find((prod) => prod.id === item.id).cantidad)+item.cantidad > (stock.find((prod) => prod.id === item.id).stock)){
-        console.log("stock sobrepasado, no se debe agregar"); //falta aplicarlo
-      }else{
-        console.log("stock disponible, no agregado aun");  //falta aplicarlo
-      }
-    }
-  }
-
-  const isInCart = (id) =>{
-    return cart.some((prod) => prod.id === id);
-  }
-
-  const cantidadCart = () =>{
-    return cart.reduce( (acc,prod) => acc + prod.cantidad , 0)
-  }
-  const totalCart = () =>{
-    return cart.reduce( (acc,prod) => acc + prod.cantidad * prod.precio , 0)
-  }
 
   return (
     <div>
-      <CartContext.Provider value={
-          {
-            cart,
-            agregarAlCarrito,
-            isInCart,
-            cantidadCart,
-            totalCart
-          }
-      }>
+      <CartProvider>
         <BrowserRouter>
           <ThemeProvider theme={theme}>
             <ResponsiveAppBar />
@@ -56,12 +26,13 @@ function App() {
                 <Route path='/' element={<ItemListContainer/>}/>
                 <Route path='/productos/:catId' element={<ItemListContainer/>}/>
                 <Route path='/detail/:itemId' element={<ItemDetailContainer/>}/>
+                <Route path='/cart' element={<Cart/>}/>
                 <Route path='*' element={<ItemListContainer/>}/>
               </Routes>
               <Footer/>
           </ThemeProvider>
         </BrowserRouter>
-      </CartContext.Provider>
+      </CartProvider>
     </div>
   );
 }
